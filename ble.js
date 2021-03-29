54a85f9a118bf4b0131bdfe793ebf3bf4defe0d3
taewook1235 <wjdxodnr1234@naver.com>
2021-03-29 10:42:43 +0900

    3

--- ble.js
+++ ble.js
@@ -6,7 +6,7 @@ by Tom Igoe
 */
 var myDevice;
 var myService = '0000FFF0-0000-1000-8000-00805F9B34FB';        // fill in a service you're looking for here
-var myCharacteristic = '0000FFF1-0000-1000-8000-00805F9B34FB';   // fill in a characteristic from the service here
+var myCharacteristic = '0000FFF2-0000-1000-8000-00805F9B34FB';   // fill in a characteristic from the service here
 
 
 function connect(){
