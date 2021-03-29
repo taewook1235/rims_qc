412260270f2155b741fb5a24c00be6a33fef7055
taewook1235 <wjdxodnr1234@naver.com>
2021-03-29 10:46:08 +0900

    3

--- ble.js
+++ ble.js
@@ -8,7 +8,8 @@ var myDevice;
 var myService = '0000FFF0-0000-1000-8000-00805F9B34FB';        // fill in a service you're looking for here
 var myCharacteristic = '0000FFF2-0000-1000-8000-00805F9B34FB';   // fill in a characteristic from the service here
 
-alert("Asdf");
+alert('Asdf');
+3
 function connect(){
  
   navigator.bluetooth.requestDevice({
