3d15a0edac8b07e7e975d62069c3e68b3b180e71
taewook1235 <wjdxodnr1234@naver.com>
2021-04-16 15:56:39 +0900

    54

--- vetpia/app/report.js
+++ vetpia/app/report.js
@@ -35,7 +35,8 @@ var g_options = {
     width:690,
     height:425,
     data: [{
-        type: "splineArea",
+        type: "Area",
+        markerType: "none",  //"circle", "square", "cross", "none"
         toolTipContent: "<b>Length: {x}</b> </br> Force: {y}",
         dataPoints: v_dataPoints
     }]
